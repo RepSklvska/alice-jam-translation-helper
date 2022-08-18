986c4d9fd7599d8cb08c17a2d5081be3940ba5b4
Alexander Sokolov <gunpowderfans@mail.ru>
2022-08-19 01:00:48 +0800

    .

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -37,10 +38,16 @@ func ReadLines(path string) ([]string, error) {
 	return lines, nil
 }
 
-func ReadDialogFileToJson(path string) error {
+func GetTextFromLine(line string) (text string) {
+	line = strings.TrimSpace(line)
+	text = line[2 : len(line)-3]
+	return text
+}
+
+func ReadDialogFileToJson(path string) (string, error) {
 	var lines, err = ReadLines(path)
 	if err != nil {
-		return err
+		return "{}", err
 	}
 
 	// 输出JSON的结构
@@ -56,39 +63,60 @@ func ReadDialogFileToJson(path string) error {
 	type Dialog struct {
 		Messages []Message `json:"messages"`
 	}
-	var dialog *Dialog = new(Dialog)
 
+	var dialog *Dialog = new(Dialog)
 	for i := 0; i < len(lines); i++ {
-		//fmt.Println(line)
+		//fmt.Println(lines[i])
 		if strings.Contains(lines[i], "CALLFUNC NAME") {
+			//fmt.Println(lines[i-1])
 			lineNumberOfName := i - 1
 			name := func() string {
 				// “CALLFUNC NAME”的上一行的引号里就是角色名称
 				lineContainsName := lines[i-1]
 				index1 := strings.Index(lineContainsName, "\"") + 1
 				index2 := strings.LastIndex(lineContainsName, "\"")
-				return lines[i][index1:index2]
+				return lineContainsName[index1:index2]
 			}()
-
+			fmt.Println(name)
 			var (
 				lineNumberOfMessageContent int
-				messageContent             []string
+				messageContent             []string = []string{}
 			)
-			for ii := i; ; i++ {
-				if strings.Contains(lines[ii], " A") {
+			for ii := i; ; ii++ {
+				if strings.LastIndex(lines[ii], " A") == len(lines[ii])-2 {
 					// 如果结尾是“ A”，则得知这是最后一行，往前找有没有结尾是“ R”的
-
+					messageContent = append(messageContent, GetTextFromLine(lines[ii]))
+					//fmt.Println(lines[ii])
+					fmt.Println(messageContent)
+					for iii := ii - 1; ; iii-- {
+						if strings.LastIndex(lines[iii], " R") == len(lines[iii])-2 {
+							//fmt.Println(lines[iii])
+							messageContent = append([]string{GetTextFromLine(lines[iii])}, messageContent...)
+							fmt.Println(messageContent)
+						} else {
+							lineNumberOfMessageContent = iii + 1
+							break
+						}
+					}
+					break
 				}
 			}
-			//dialog.Messages = append(dialog.Messages)
+			var message *Message = new(Message)
+			message.Line = lineNumberOfName
+			message.Name = name
+			message.Message.Line = lineNumberOfMessageContent
+			message.Message.Text = messageContent
+			dialog.Messages = append(dialog.Messages)
 		}
 	}
+	fmt.Println(dialog)
+	//jsonBytes, err := json.Marshal(dialog)
 
-	return nil
+	return "", nil
 }
 
 func main() {
-	os.Chdir(BasePath)
-	os.Chdir(ScenarioTextPath)
+	//os.Chdir(BasePath)
+	//os.Chdir(ScenarioTextPath)
 	ReadDialogFileToJson("./samples/195/僔僫儕僆HTLO_S001_A.jam")
 }
